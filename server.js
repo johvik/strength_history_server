8a93b3b7b475d4cd0ee036ddcab09948d6278da7
Mazo <john.viklund@gmail.com>
2013-07-20 13:14:37 +0200

    Added some test stuff

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
+app.use(express.bodyParser());
 app.use(express.compress());
 app.use(express.static(__dirname + '/public'));
 
@@ -9,6 +10,19 @@ app.get('/', function(req, res) {
 	res.render(__dirname + '/index.jade', {
 		title : 'Strength History'
 	});
+});
+
+app.post('/test', function(req, res) {
+	if (req.body.user == 'test' && req.body.password == 'test') {
+		var data = {
+			test : "test1",
+			tt : 2.5,
+			array : [ 2, 43, 4, 3 ]
+		};
+		res.json(data);
+	} else {
+		res.send(401);
+	}
 });
 
 app.listen(process.env.VCAP_APP_PORT || 80);
