76a6ba3e838b5d1ae0831f1548cdb19b00c6bd1a
Mazo <john.viklund@gmail.com>
2013-07-22 19:36:17 +0200

    Small fixes

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,17 @@
 var passport = require('passport');
 
 exports.login = function(req, res, next) {
-	passport.authenticate('local', function(err, user, info) {
-		if (err) {
-			return next(err);
+	passport.authenticate('local', function(err1, user, info) {
+		if (err1) {
+			return next(err1);
 		}
 		if (!user) {
 			req.session.message = info.message;
 			return res.redirect('/');
 		}
-		req.logIn(user, function(err) {
-			if (err) {
-				return next(err);
+		req.logIn(user, function(err2) {
+			if (err2) {
+				return next(err2);
 			}
 			return res.redirect('/');
 		});
