f88f5003996a96d85555437d9b128d9ef5f24324
Mazo <john.viklund@gmail.com>
2013-07-24 20:19:05 +0200

    Added some db models

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,17 @@
+var Weight = require('../lib/db/weight').Model;
+var Exercise = require('../lib/db/exercise').Model;
+var Workout = require('../lib/db/workout').Model;
+
 exports.test = function(req, res) {
-	res.send('Hello ' + req.user.username + '!');
+	Weight.latest(req.user._id, function(err, doc) {
+		console.log(doc);
+	});
+	// Weight.addTest(req.user._id);
+	// Exercise.addTest(req.user._id);
+	// Workout.addTest(req.user._id);
+	Weight.find({
+		user : req.user._id
+	}, function(err, docs) {
+		res.send('Hello ' + req.user.username + '! ' + docs);
+	});
 };
