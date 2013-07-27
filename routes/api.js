899d3e7b6438c967b3cd56a04e81a456465ed077
Mazo <john.viklund@gmail.com>
2013-07-27 10:27:50 +0200

    Latest function to workout

--- routes/api.js
+++ routes/api.js
@@ -5,17 +5,33 @@ var WorkoutData = require('../lib/db/workoutdata').Model;
 
 exports.test = function(req, res) {
 	var userid = req.user._id;
+	Exercise.findOne({
+		user : userid
+	}, function(err, doc) {
+		//console.log(err);
+		//console.log(doc);
+		if (doc) {
+			//Workout.addTest(userid, doc._id);
+		}
+	});
 	Workout.findOne({
 		user : userid
 	}, function(err1, doc1) {
-		Exercise.findOne({
-			user : userid
-		}, function(err2, doc2) {
-			WorkoutData.addTest(userid, doc1._id, doc2._id);
-			doc2.latest(userid, function(err3, doc3) {
-				console.log(doc3);
+		if (doc1) {
+			Exercise.findOne({
+				user : userid
+			}, function(err2, doc2) {
+				WorkoutData.addTest(userid, doc1._id, doc2._id);
+				doc2.latest(userid, function(err3, doc3) {
+					// console.log(doc3);
+					doc1.latest(userid, function(err4, doc4) {
+						console.log(err4);
+						console.log(doc4);
+						Workout.addTest(userid, doc2._id);
+					});
+				});
 			});
-		});
+		}
 	});
 	Weight.latest(userid, function(err, doc) {
 		if (doc) {
