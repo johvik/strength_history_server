a862ccfdf784204155285c6e3c09442c8fa36647
Mazo <john.viklund@gmail.com>
2013-07-28 22:02:11 +0200

    Fixed structure of app.js and removed test code

--- app.js
+++ app.js
@@ -32,31 +32,32 @@ if ('development' == app.get('env')) {
 	app.use(express.errorHandler());
 }
 
-app.del('/exercise/:id', pass.ensureAuthenticated, exerciseApi.del);
-app.del('/weight/:id', pass.ensureAuthenticated, weightApi.del);
+// Login routes
+app.get('/', routes.index);
+app.get('/logout', user.logout);
+app.post('/login', user.login);
 
+app.get('/test', pass.ensureAuthenticated, api.test);
+
+// Exercise API
+app.del('/exercise/:id', pass.ensureAuthenticated, exerciseApi.del);
 app.get('/exercise/latest/:id', pass.ensureAuthenticated, exerciseApi.getLatest);
 app.get('/exercise/pages/:id', pass.ensureAuthenticated, exerciseApi.getPage);
 app.get('/exercise/pages', pass.ensureAuthenticated, exerciseApi.getPages);
 app.get('/exercise', pass.ensureAuthenticated, exerciseApi.get);
 app.get('/exercise/:id', pass.ensureAuthenticated, exerciseApi.getId);
+app.post('/exercise', pass.ensureAuthenticated, exerciseApi.post);
+app.put('/exercise/:id', pass.ensureAuthenticated, exerciseApi.put);
+
+// Weight API
+app.del('/weight/:id', pass.ensureAuthenticated, weightApi.del);
 app.get('/weight/latest', pass.ensureAuthenticated, weightApi.getLatest);
 app.get('/weight/pages/:id', pass.ensureAuthenticated, weightApi.getPage);
 app.get('/weight/pages', pass.ensureAuthenticated, weightApi.getPages);
 app.get('/weight', pass.ensureAuthenticated, weightApi.get);
 app.get('/weight/:id', pass.ensureAuthenticated, weightApi.getId);
-
-app.post('/exercise', pass.ensureAuthenticated, exerciseApi.post);
 app.post('/weight', pass.ensureAuthenticated, weightApi.post);
-
-app.put('/exercise/:id', pass.ensureAuthenticated, exerciseApi.put);
 app.put('/weight/:id', pass.ensureAuthenticated, weightApi.put);
-
-app.get('/', routes.index);
-app.get('/logout', user.logout);
-app.get('/test', pass.ensureAuthenticated, api.test);
-
-app.post('/login', user.login);
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Strength History Web listening on port ' + app.get('port'));
