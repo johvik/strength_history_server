14ba7efdd5815fd8b45d2aba2f70c0b876f38d91
Mazo <john.viklund@gmail.com>
2013-09-16 18:52:48 +0200

    Added argument to build+copy before starting

--- start_with_mongo.bat
+++ start_with_mongo.bat
@@ -1,4 +1,15 @@
 @ECHO OFF
+SETLOCAL
+
+if not "%1%"=="b" goto nobuild
+rmdir /s /q public\
+cd ..\strength_history_web\build\
+call build.bat
+xcopy /e /i output ..\..\strength_history_server\public
+
+:nobuild
+ENDLOCAL
+
 start cmd /c "cd C:\mongodb\bin && mongod.exe"
 start cmd /c "cd C:\mongodb\bin && mongo.exe db"
 node app.js
