8584f29de6cbd2fd0aa03d2af65de5088027e1b0
johvik <john.viklund@gmail.com>
2013-11-16 13:15:52 +0100

    Removed appfog and changed mongo config

--- config/sample_config.js
+++ config/sample_config.js
@@ -1,16 +1,9 @@
 // Use this as a template for config.js
-exports.SERVER_PORT = 8080;
+exports.SERVER_PORT = 8080; // Port of your server
 exports.SERVER_ADDRESS = 'http://localhost:' + exports.SERVER_PORT; // The root address of your server
 exports.SESSION_SECRET = 'secret'; // Used for express.session
 // This email will be used to send activation e-mails
 exports.EMAIL_ACCOUNT = 'some.email@localhost';
 exports.EMAIL_PASSWORD = 'email password';
-// Settings for MongoDB
-exports.MONGODB = {
-  'hostname' : 'localhost',
-  'port' : 27017,
-  'username' : '',
-  'password' : '',
-  'name' : '',
-  'db' : 'db'
-};
+// Address to MongoDB
+exports.MONGODB = 'mongodb://localhost:27017/db';
